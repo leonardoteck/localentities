6a178c67cc4e9208360dd44081856789d94b56a1
Leonardo Costa <leonardoteck@gmail.com>
2018-05-04 00:47:40 -0300

    Testing environment configured

---
+++ test/validator-spec.js
@@ -0,0 +1,11 @@
+var assert = chai.assert;
+
+function validator() {
+    
+}
+
+describe('A Validator', function () {
+    it('will return error.nonpositive for not strictly positive numbers', function () {
+        assert.deepEqual(validator(0), ['error.nonpositive']);
+    });
+});
